2972ac0c5cdee8de33384c8c7bb35cb8e64dcd3d
ngrisanti <54860668+ngrisanti@users.noreply.github.com>
2021-05-26 19:47:30 -0700

    cleaned up encoding, changed return type to numpy array, added comments

--- encoding.py
+++ encoding.py
@@ -9,25 +9,13 @@ from classes.Wire import *
 from classes.Arrow import *
 from classes.Cell import *
 
-def flatten_list(listoflist):
-    flatten_trap = list(itertools.chain(*listoflist))
-    return flatten_trap
-
 def encoding(trap):
+    """
+    Takes a trap and returns a 1x12 numpy int array encoding it
+    Each sequence of three items in the array represents a row of the trap grid
+    """
     encoded = []
-    flatten_trap = flatten_list(trap)
+    flatten_trap = list(itertools.chain(*trap))
     for cell in flatten_trap:
         encoded.append(ca.find_index(cell))
-    return encoded
-
-#testing#
-'''
-trap7 = [
-    [Arrow(0,0, None, angleType=AngleType.racute, rotationType=RotationType.up, thickType=ThickType.wide), Floor(1,0, None),  Floor(2,0, None)],
-    [Wire(0,1,None, angleType=AngleType.straight, rotationType=RotationType.up, thickType=ThickType.wide), Food(1,1, None), Arrow(2,1, None, angleType=AngleType.lright, rotationType=RotationType.up, thickType=ThickType.wide)],
-    [Wire(0,2,None, angleType=AngleType.straight, rotationType=RotationType.up, thickType=ThickType.wide), Floor(1,2, None), Wire(2,2, None, angleType=AngleType.straight, rotationType=RotationType.up, thickType=ThickType.wide)],
-    [Wire(0,3,None, angleType=AngleType.rright, rotationType=RotationType.left, thickType=ThickType.wide), Door(1,3, None), Wire(2,3, None, angleType=AngleType.lright, rotationType=RotationType.right, thickType=ThickType.wide)]
-]
-print(flatten_list(trap7))
-print(encoding(trap7))
-'''
+    return np.array(encoded)
