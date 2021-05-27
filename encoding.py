23ba4cc9cf7a1e959eca51374dea4fbfba510509
ngrisanti <54860668+ngrisanti@users.noreply.github.com>
2021-05-27 01:42:03 -0700

    wrote genetic algorithm functions

--- encoding.py
+++ encoding.py
@@ -8,8 +8,14 @@ from classes.Dirt import *
 from classes.Wire import *
 from classes.Arrow import *
 from classes.Cell import *
+from designedTraps import *
 
-def encoding(trap):
+## IMPORTANT: For every encoded trap, the following must be true to be a valid config:
+    ## trap[4] = 2
+    ## trap[7] = 0
+    ## trap[10] = 1
+
+def singleEncoding(trap):
     """
     Takes a trap and returns a 1x12 numpy int array encoding it
     Each sequence of three items in the array represents a row of the trap grid
@@ -19,3 +25,40 @@ def encoding(trap):
     for cell in flatten_trap:
         encoded.append(ca.find_index(cell))
     return np.array(encoded)
+
+def listEncoding(traps):
+    """Given a list of n non-encoded traps, return a nx12 numpy array with the encoded versions of the traps"""
+    # encode traps
+    encodedTraps = []
+    for trap in traps:
+        encodedTraps.append(singleEncoding(trap))
+    return np.array(encodedTraps)
+
+def checkDifferences(traps):
+    """Given a list of non-encoded traps, determine if there are differences between the columns
+    across all encoded traps in the list. Returns numpy array with 'diff' in any column with at least one difference,
+    otherwise puts the shared value in that position."""
+    result = []
+    traps = listEncoding(traps)
+    for i in range(12):
+        different = False
+        firstVal = traps[0][i]
+        for j in range(len(traps)):
+            if firstVal != traps[j][i]:
+                different = True
+                break
+            else:
+                continue
+        if different:
+            result.append('diff')
+        else:
+            result.append(firstVal)
+    return result
+
+def singleDecoding():
+    """Takes an encoded trap and decodes it back into normal form"""
+    pass
+
+def listDecoding():
+    """Takes an list of encoded traps and decodes its elements back into normal form"""
+    pass
--- geneticAlgorithm.py
+++ geneticAlgorithm.py
@@ -2,6 +2,13 @@ import simulation as sim
 import classes.Trap as TrapClass
 import algorithms as alg
 import numpy as np
+import random
+from encoding import *
+
+# Nick: I am not sure what the sample space is from the encoding, so I am using the ints from 0-56 for testing.
+    # The code I wrote does not depend on any particular sample space so no changes should need to be made.
+space = [x for x in range(57)]
+population = traps
 
 def randomFitness(configuration):
     """Assigns a random fitness to each configuration (choosing uniformly at random)"""
@@ -25,12 +32,12 @@ def functionalFitness(configuration, numSimulations = 100):
     proportion = numberAlive / numSimulations
     stderr = np.sqrt(proportion * (1 - proportion) / numSimulations)
 
-    upperCI = proportion + 1.96 * stderr
-    lowerCI = proportion - 1.96 * stderr
+    # upperCI = proportion + 1.96 * stderr
+    # lowerCI = proportion - 1.96 * stderr
 
-    print("Proportion: ", proportion)
-    print("Std Error: ", round(stderr, 3))
-    print("CI: [", round(lowerCI, 3), ", ", round(upperCI, 3), "]")
+    # print("Proportion: ", proportion)
+    # print("Std Error: ", round(stderr, 3))
+    # print("CI: [", round(lowerCI, 3), ", ", round(upperCI, 3), "]")
     return proportion
 
 def coherentFitness(configuration):
@@ -39,33 +46,82 @@ def coherentFitness(configuration):
 
 def initializePopulation(searchSpace, populationSize):
     """Initializes the population by sampling from the search space"""
-    pass
+    population = []
+    for i in range(populationSize):
+        member = []
+        for j in range(12):
+            member.append(np.random.choice(searchSpace))
+        print(member)
+        population.append(member)
+    return np.array(population)
 
-def checkTermination(fitnesses):
-    """Checks termination as a function of the given fitnesses"""
-    pass
+def checkTermination(fitnesses, measure, threshold):
+    """Checks termination as a function of the given fitnesses.
+       Stops when measure ('all', 'mean', 'median') of fitness meets threshold
+    """
+    if measure == 'all':
+        for i in range(len(fitnesses)):
+            if fitnesses[i] < threshold:
+                return False
+            else:
+                continue
+        return True
+    elif (measure == 'mean' and np.mean(fitnesses) >= threshold) \
+        or (measure == 'median' and np.median(fitnesses) >= threshold):
+        return True
+    elif (measure == 'mean' and np.mean(fitnesses) < threshold) \
+        or (measure == 'median' and np.median(fitnesses) < threshold):
+        return False
+    else:
+        raise ValueError('measure must be \'all\', \'mean\', or \'median\'.')
 
-def selectionFunc(population):
-    """Takes a sample from the population to replicate from"""
-    pass
+def selectionFunc(population, fitnessFunc):
+    """Performs a roulette wheel-style selection process, 
+        giving greater weight to members with greater fitnesses"""
+    fitnesses = np.array([])
+    for member in population:
+        fit = fitnessFunc(member)
+        fitnesses = np.append(fitnesses, fit)
+    sum = np.sum(fitnesses)
+    scaledFitnesses = fitnesses / sum
+    return random.choices(population, weights=scaledFitnesses, k=len(population))
 
-def recombinationFunc(population):
-    """Recombines members of the population to form a new population"""
-    pass
+def crossoverFunc(population):
+    """Crosses-over two members of the population to form a new population"""
+    encodedPop = listEncoding(population)
+    member1_i = random.randrange(0, len(encodedPop), 1)
+    member2_i = random.randrange(0, len(encodedPop), 1)
+    while member2_i == member1_i:
+        member2_i = random.randrange(0, len(encodedPop), 1)
+    # cut occurs between (index - 1) and index
+    index = random.randrange(1, len(encodedPop[member1_i]), 1)
+    left1 = encodedPop[member1_i][:index]
+    right1 = encodedPop[member1_i][index:]
+    left2 = encodedPop[member2_i][:index]
+    right2 = encodedPop[member2_i][index:]
+    encodedPop[member1_i]=(np.append(left1, right2))
+    encodedPop[member2_i]=(np.append(left2, right1))
+    return encodedPop
 
-def mutationFunc(population):
-    """Mutates members of the population at random to introduce genetic diversity"""
-    pass
+def mutationFunc(searchSpace, encodedPop):
+    """Performs one a single point mutation on a random member of the population"""
+    member_i = random.randrange(0, len(encodedPop), 1)
+    index = random.randrange(0, len(encodedPop[member_i]), 1)
+    # Ensure the flipped tile is not one of the required fixed tiles
+    while (index == 4) or (index == 7) or (index == 10):
+        index = random.randrange(0, len(encodedPop[member_i]), 1)
+    encodedPop[member_i][index] = np.random.choice(searchSpace)
+    return encodedPop
 
-def geneticAlgorithm(searchSpace, fitnessFunc):
+def geneticAlgorithm(searchSpace, fitnessFunc, measure, threshold):
     """Finds a near-optimal solution in the search space using the given fitness function"""
     population = initializePopulation(searchSpace, 20)
-    fitnesses = [fitnessFunc(individual) for individual in population]
+    fitnesses = [fitnessFunc(member) for member in population]
 
-    while not checkTermination(fitnesses):
-        population = selectionFunc(population)
-        population = recombinationFunc(population)
-        population = mutationFunc(population)
-
+    while not checkTermination(fitnesses, measure, threshold):
+        population = selectionFunc(population, fitnessFunc)
+        encodedPop = crossoverFunc(population)
+        encodedPop = mutationFunc(searchSpace, encodedPop)
+        population = listDecoding(encodedPop)
         fitnesses = fitnessFunc(population)
-    pass
+    return population
