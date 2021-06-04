dafa2026873046887137cd7c1162e856dc700e0b
Anshul Kamath <anshulkam@gmail.com>
2021-06-04 19:20:58 -0400

    small edits

--- benchmark.py
+++ benchmark.py
@@ -41,28 +41,29 @@ def functionalFitness(configuration, numSimulations = 5000):
 # Add 63 random traps to trap list
 traps.extend(initializePopulation(constants.CELL_ALPHABET, 63))
 
+fileLines = []
+for i, trap in enumerate(traps):
+    # Calculating time changes
+    startTime = time.time()
+    analyticalSol = analytical.trapLethality(trap)
+    midTime = time.time()
+    simulatedSol = functionalFitness(np.array(trap))
+    endTime = time.time()
+
+    analyticTimeMs = (midTime - startTime) * 1e3
+    simulatedTimeMs = (endTime - startTime) * 1e3
+    fileLines.append('Trap {}:\n'.format(i))
+    fileLines.append('Difference\t\t\t\t:\t{}\n'.format(round(analyticalSol - simulatedSol, 6)))
+    fileLines.append('Time Analytical (ms)\t:\t{}\n'.format(round(analyticTimeMs, 3)))
+    fileLines.append('Time Simulated  (ms)\t:\t{}\n'.format(round(simulatedTimeMs, 3)))
+    fileLines.append('Factor Difference\t\t:\t{}\n\n'.format(round(simulatedTimeMs / analyticTimeMs, 3)))
+
+    print('Trap {}:'.format(i))
+    print('Difference\t\t:\t{}'.format(round(analyticalSol - simulatedSol, 6)))
+    print('Time Analytical (ms)\t:\t{}'.format(round(analyticTimeMs, 3)))
+    print('Time Simulated  (ms)\t:\t{}'.format(round(simulatedTimeMs, 3)))
+    print('Factor Difference\t:\t{}\n\n'.format(round(simulatedTimeMs / analyticTimeMs, 3)))
+
 with open('performance.txt', 'w+') as out:
-    fileLines = []
-    for i, trap in enumerate(traps):
-        # Calculating time changes
-        startTime = time.time()
-        analyticalSol = analytical.trapLethality(trap)
-        midTime = time.time()
-        simulatedSol = functionalFitness(np.array(trap))
-        endTime = time.time()
-
-        analyticTimeMs = (midTime - startTime) * 1e3
-        simulatedTimeMs = (endTime - startTime) * 1e3
-        fileLines.append('Trap {}:\n'.format(i))
-        fileLines.append('Difference\t\t\t\t:\t{}\n'.format(round(analyticalSol - simulatedSol, 6)))
-        fileLines.append('Time Analytical (ms)\t:\t{}\n'.format(round(analyticTimeMs, 3)))
-        fileLines.append('Time Simulated  (ms)\t:\t{}\n'.format(round(simulatedTimeMs, 3)))
-        fileLines.append('Factor Difference\t\t:\t{}\n\n'.format(round(simulatedTimeMs / analyticTimeMs, 3)))
-
-        print('Trap {}:'.format(i))
-        print('Difference\t\t:\t{}'.format(round(analyticalSol - simulatedSol, 6)))
-        print('Time Analytical (ms)\t:\t{}'.format(round(analyticTimeMs, 3)))
-        print('Time Simulated  (ms)\t:\t{}'.format(round(simulatedTimeMs, 3)))
-        print('Factor Difference\t:\t{}\n\n'.format(round(simulatedTimeMs / analyticTimeMs, 3)))
-
     out.writelines(fileLines)
+    out.close()
