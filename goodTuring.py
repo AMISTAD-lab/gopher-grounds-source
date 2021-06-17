f83122eca78dce60d6269b069cdb207438f7649e
Sadie-Zhao <sadiee.zhao@gmail.com>
2021-06-17 14:00:56 -0400

    WIP:  implemented Simple Good Turing smoothing skeleton

--- goodTuring.py
+++ goodTuring.py
@@ -4,6 +4,8 @@ This file contains all the necessary functions to apply a Simple Good Turing smo
 import copy
 import csv
 import numpy as np
+from numpy.core.fromnumeric import sort
+from scipy import linalg
 
 def createFreqOfFreqs(fitnessFunction):
     """
@@ -82,6 +84,10 @@ def loadFoF(fitnessFunc):
         reader = csv.reader(out)
 
         for row in reader:
+            # Skip empty row
+            if len(row) == 0:
+                continue
+
             if row[0] == 'Frequency':
                 continue
 
@@ -89,7 +95,7 @@ def loadFoF(fitnessFunc):
 
     return compiledDict
 
-def getProbability(configuration, freqDict, fofDict):
+def turingProb(configuration, freqDict, fofDict):
     """
     Returns the frequency of a given configuration, the dictionary of frequencies,
     and the dictionary of frequencies of frequencies
@@ -103,5 +109,72 @@ def getProbability(configuration, freqDict, fofDict):
 
     return r_star / N
 
+def SGTProbs(freqDict, fofDict, confidenceLevel=1.96):
+    """
+    Returns a dictionary mapping the observed frequency to the corresponding smoothed proabilities
+    That is, a dictionary {freq: SGTProb}
+    """
+    probDict = {}
+    N = np.sum([freq * fofDict[freq] for freq in fofDict])
+    sortedFreq = sorted(fofDict.keys()) #a array of sorted freqs
+    print(N == sum(freqDict.values()))
+
+    probDict[0] = fofDict[1] / N
+
+    # Compute the dictionary of {r: Z_r}
+    Z = computeZ(fofDict, sortedFreq)
+
+     # Compute a linear regression of log(Z[r]) on log(r)
+    a, b = logLinearReg(Z)
+
+    # Simple Good Turing
+    rSmoothed = {}
+    useLinear = False
+    for r in sortedFreq:
+        r_linear = r * (1 + 1/r)^(b + 1) 
+
+        # If we started using LGT, continue use it
+
+        # If we didn't start use LGT (still using Turing estimate), check 1.the difference between two estimates, 2.if N_r+1==0, switch to LGT
+    pass
+
+def computeZ(fofDict, sortedFreq):
+    """
+    Returns a dictionary of {r: Z_r} for every frequency r in sorted order (by r)
+    For each r, Z[r] = Z_r = N_r / 0.5(t-q) where q, r, t are successive indices of non-zero fof values
+    """
+    Z = {}
+    for index, r in enumerate(sortedFreq):
+        # Find q
+        if index == 0:
+            q = 0
+        else:
+            q = sortedFreq[index - 1]
+        # Find t
+        if index == len(sortedFreq) -  1:
+            t = r + (r - q)        # Not sure what this should be
+        else:
+            t = sortedFreq[index + 1]
+    
+        Z[r] = fofDict[r] / (0.5 * (t - q))
+    return Z
+
+def logLinearReg(zDict):
+    x = np.log(np.fromiter(zDict.keys(), dtype=int))
+    y = np.log(np.fromiter(zDict.values(), dtype=float))
+    A = np.vstack([x, np.ones(len(x))]).T
+    b, a = np.linalg.lstsq(A, y, rcond=None)[0]
+    print('Regression: log(z) = {b}*log(r) + {a}'.format(b = b, a = a))
+    if b > -1.0:
+        print('Warning: slope b > -1.0')
+    return a, b
+
+
 freqDict = loadFrequencies('coherence')
+print("finish loading freqDict")
+print()
 fofDict = loadFoF('coherence')
+sortedFreq = sorted(fofDict.keys())
+Z = computeZ(fofDict, sortedFreq)
+logLinearReg(Z)
+
