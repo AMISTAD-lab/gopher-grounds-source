461c8e186a85ba5ab35b3e22543847286ebfeac8
Anshul Kamath <anshulkam@gmail.com>
2021-06-17 10:59:24 -0400

    good turing changes

--- goodTuring.py
+++ goodTuring.py
@@ -3,7 +3,7 @@ This file contains all the necessary functions to apply a Simple Good Turing smo
 """
 import copy
 import csv
-import pandas as pd
+import numpy as np
 
 def createFreqOfFreqs(fitnessFunction):
     """
@@ -50,4 +50,58 @@ def createFreqOfFreqs(fitnessFunction):
 
     return freqOfFreqs
 
-createFreqOfFreqs('coherence')
+def loadFrequencies(fitnessFunc):
+    """
+    Returns a dictionary of { 'strEncoding': frequency } of all frequencies of a given fitness function
+    """
+    outputPath = './frequencies/{}/{}FreqsCompiled.csv'.format(fitnessFunc, fitnessFunc)
+
+    compiledDict = {}
+    with open(outputPath, 'r') as out:
+        reader = csv.reader(out)
+
+        for row in reader:
+            if row[1] == 'Freq':
+                continue
+
+            if row[0] in compiledDict:
+                raise Exception('Duplicate trap {}'.format(row[0]))
+            
+            compiledDict[row[0]] = int(row[1])
+    
+    return compiledDict
+
+def loadFoF(fitnessFunc):
+    """
+    Returns a dictionary of all frequency of frequencies of a given fitness function
+    """
+    path = './frequencies/{}/{}FreqOfFreqs.csv'.format(fitnessFunc, fitnessFunc)
+
+    compiledDict = {}
+    with open(path, 'r') as out:
+        reader = csv.reader(out)
+
+        for row in reader:
+            if row[0] == 'Frequency':
+                continue
+
+            compiledDict[int(row[0])] = int(row[1])
+
+    return compiledDict
+
+def getProbability(configuration, freqDict, fofDict):
+    """
+    Returns the frequency of a given configuration, the dictionary of frequencies,
+    and the dictionary of frequencies of frequencies
+    """
+    strEncoding = np.array2string(np.array(configuration))
+    r = 0 if strEncoding not in freqDict else freqDict[strEncoding]
+
+    r_star = (r + 1) * fofDict[r + 1] / fofDict[r]
+
+    N = np.sum([freq * fofDict[freq] for freq in fofDict])
+
+    return r_star / N
+
+freqDict = loadFrequencies('coherence')
+fofDict = loadFoF('coherence')
