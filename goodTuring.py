dacca92f143782548628cc3d9c92a27af1be0d59
Sadie-Zhao <sadiee.zhao@gmail.com>
2021-07-08 12:43:36 -0400

    WIP: changes to goodTuring

--- goodTuring.py
+++ goodTuring.py
@@ -8,56 +8,11 @@ import pprint
 import database.library as dbLibrary
 import geneticAlgorithm.constants as constants
 
-def createFreqOfFreqs(fitnessFunction):
-    """
-    Returns a dictionary of all frequencies of frequencies of the given input file.
-    Additionally, writes the data to a file to prevent having to run the expensive operation again
-    """
-    inputPath = './frequencies/{}/{}FreqsCompiled.csv'.format(fitnessFunction, fitnessFunction)
-    outputPath = './frequencies/{}/{}FreqOfFreqs.csv'.format(fitnessFunction, fitnessFunction)
-
-    # Maintain a dictionary of counts
-    freqOfFreqs = {}
-
-    with open(inputPath, 'r', newline='') as out:
-        reader = csv.reader(out)
-
-        for row in reader:
-            if row[1] == 'Freq':
-                continue
-
-            currFreq = int(row[1])
-
-            if currFreq not in freqOfFreqs:
-                freqOfFreqs[currFreq] = 0
-            
-            freqOfFreqs[currFreq] += 1
-        out.close()
-
-    # Write the data to a file
-    with open(outputPath, 'w+', newline='') as out:
-        writer = csv.writer(out)
-        writeData = [['Frequency', 'Frequency_of_Frequency']]
-        currI = 1
-
-        copyDict = copy.copy(freqOfFreqs)
-        while copyDict:
-            if currI in copyDict:
-                writeData.append([str(currI), str(copyDict[currI])])
-                del copyDict[currI]
-            
-            currI += 1
-
-        writer.writerows(writeData)
-        out.close()
-
-    return freqOfFreqs
-
 def loadFoF(fitnessFunc):
     """
     Returns a dictionary of all frequency of frequencies of a given fitness function
     """
-    path = './frequencies/{}/{}FreqOfFreqs.csv'.format(fitnessFunc, fitnessFunc)
+    path = constants.fofPath.format(fitnessFunc,'')
 
     compiledDict = {}
     with open(path, 'r', newline='') as out:
@@ -98,14 +53,14 @@ def getProbDict(fofDict, confidenceLevel=1.65):
     # Simple Good Turing
     r_smoothed = {}
     useLinear = False
-    for r in sortedFreq:
+
+    for index, r in enumerate(sortedFreq):
         r_linear = r * (1 + 1 / r) ** (b + 1)  # estimates of r using LGT
 
         # If we didn't start use LGT (still using Turing estimate) 
         if not useLinear:
             # 1. if N_r+1==0, switch to LGT
             if (r + 1) not in fofDict:
-                print("reached unobserved frequency before crossing the 'smoothing threshold.'")
                 useLinear = True
                 r_smoothed[r] = r_linear
                 continue
@@ -168,8 +123,6 @@ def logLinearReg(zDict):
     A = np.vstack([x, np.ones(len(x))]).T   
     b, a = np.linalg.lstsq(A, y, rcond=None)[0]
 
-    print('Regression: log(z) = {} * log(r) + {}'.format(round(b, 3), round(a, 3)))
-
     if b > -1.0:
         raise Exception('Warning: slope b > -1.0')
 
@@ -180,7 +133,8 @@ def getSmoothedProb(configuration, fitnessFunc):
     Returns the SGT-smoothed probability of a given configuration
     """
     probDict = getProbDict(loadFoF(fitnessFunc))
-    r = dbLibrary.getTrapFreq(configuration, fitnessFunc)
+    r = 0 if dbLibrary.getTrapFreq(configuration, fitnessFunc) == None else dbLibrary.getTrapFreq(configuration, fitnessFunc)
+    print("frequency=", r)
     return probDict[r]
 
 def testSGT(configuration, function = 'coherence'):
@@ -198,3 +152,5 @@ def testSGT(configuration, function = 'coherence'):
 
     print("This is the probability of a certain trap:")
     print(getSmoothedProb(configuration, function))
+
+testSGT('[67  3  8 50  1 31 20  2 68 22  0 83]', 'functional')
