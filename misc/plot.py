cabea6789e8920e0c588ae47a0a046b226a75693
ngrisanti <54860668+ngrisanti@users.noreply.github.com>
2021-07-15 00:14:35 -0700

    generated new plots for blog post

--- misc/plot.py
+++ misc/plot.py
@@ -60,3 +60,54 @@ def create1DHeatmap(df: pd.DataFrame, title='', x_lab='', y_lab='Frequency', lab
     ax.set_ylabel(y_lab, fontsize=label_size, labelpad=10)
 
     pass
+
+def multiobjectivePlot(df: pd.DataFrame, x='functional_fitness', y='coherent_fitness', jitter=False, xlabel='', ylabel='', title='', labelLoc='upper right', box_index=None):
+    """
+    Plots function vs coherence data from a df. 
+    Creates shaded box with upper-right corner at trap with index 'box_index.'
+    """
+    plt.rcParams.update({'font.size':15})
+    fig, ax = plt.subplots()
+
+    x = np.array(df[x])
+    y = np.array(df[y])
+
+    if jitter:
+        x = addJitter(x)
+        y = addJitter(y)
+
+    ax.add_patch(Rectangle((0, 0), x[box_index], y[box_index],
+             facecolor = 'gray',
+             fill=True,
+             lw=1,
+             alpha=0.5))
+    plt.scatter(x, y, s=100)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.title(title)
+    plt.show()
+
+def barPlot():    
+    # creating the dataset
+    data = {'random': 0.01, 'functional': 0.01, 'coherent': 1, 'multiobjective': 0.77, 'binary distance': 0.01}
+    funcs = list(data.keys())
+    diffs = list(data.values())
+    
+    fig = plt.figure(figsize = (10, 4))
+    
+    # creating the bar plot
+    plt.bar(funcs, diffs, color ='orange',
+            width = 0.4)
+
+    addlabels(funcs, diffs)
+    
+    plt.xlabel("FITNESS FUNCTION")
+    plt.ylabel("% OF TRAPS DETERMINED HUMAN-DESIGNED")
+    plt.show()
+
+def addlabels(x,y):
+    for i in range(len(x)):
+        if y[i] == 0.01:
+            plt.text(i,y[i],0,ha='center')
+        else:
+            plt.text(i,y[i],y[i],ha='center')
