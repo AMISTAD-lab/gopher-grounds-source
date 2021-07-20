a1353d28fb3fcdc972e193fe8ef3141a2730253f
Anshul Kamath <anshulkam@gmail.com>
2021-07-20 15:47:06 -0700

    modified plotting script

--- misc/plotScripts.py
+++ misc/plotScripts.py
@@ -1,5 +1,6 @@
-import matplotlib.pyplot as plt
-import database.plot as plot2
+# import matplotlib.pyplot as plt
+# import database.plot as plot2
+import misc.visualization as vis
 
 # plot2.createLethalityCoherenceHeatMap('coherence', log=True, save=True)
 # plot2.createLethalityCoherenceHeatMap('random', log=True, save=True)
@@ -8,14 +9,18 @@ import database.plot as plot2
 # plot2.createLethalityCoherenceHeatMap('binary-distance', log=True, save=True)
 # plot2.createLethalityCoherenceHeatMap('uniform-random', log=True, save=True)
 
-# plot2.createVectorMaps('lethality')
-# plot2.createVectorMaps('coherence', save=True)
+# plot2.createVectorMaps('lethality', save=True, name='VectorMapProd')
+# plot2.createVectorMaps('coherence', save=True, name='VectorMapProd')
 
 # plot2.createLethVCohGenScatter('multiobjective')
 
 # plot2.createGenerationBoxPlot(save=True)
 
 # plot2.createAverageOptimalFitnessLinePlot(save=True)
-plot2.createAverageGenerationLinePlot(cumulative=False, end=500, save=True)
+# plot2.createAverageGenerationLinePlot(cumulative=False, end=500, save=True)
+
+vis.convertTrapToImage('[47, 6, 86, 25, 1, 29, 26, 2, 62, 72, 0, 9]', 'example', save=True)
+vis.createAnnotatedTrap(save=True, show=False)
+vis.createSplitTrap(5, save=True, show=False)
 
 # plt.show()
